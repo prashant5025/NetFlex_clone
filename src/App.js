d87e9fd448ef1399d06c7c8ba2f3783187005b04
Prashant Singh <psingh5025615@gmail.com>
2022-09-15 06:07:37 +0530

    "initial commit"

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Route,Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      {/* Page Content  */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Main.jsx
@@ -0,0 +1,25 @@
+import axios from 'axios'
+import React,{ useEffect, useState } from 'react'
+import requests from '../Request'
+
+
+const Main = () => {
+    const [movies,setMovies] = useState([])
+
+    const movie = movies[Math.floor(Math.random() * movies.length)]
+    useEffect(() => {
+        axios.get(requests.requestPopular).then((response) => {
+            setMovies(response.data.results)
+        })
+    },[])
+    console.log(movie)
+    
+
+  return (
+    <div>
+      Main
+    </div>
+  )
+}
+
+export default Main;
