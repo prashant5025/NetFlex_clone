775d7b9dfa0c5ed42994fbef3ec5188f8ade4c94
Prashant Singh <psingh5025615@gmail.com>
2022-09-15 08:18:11 +0530

    UI done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import Main from '../components/Main'
+import Row from '../components/Row'
+import requests from '../Request'
 const Home = () => {
   return (
     <div>
       <Main/>
+      <Row rowID='1' title='Up Coming' fetchURl={requests.requestUpcoming}/>
+      <Row rowID='2' title='Popular' fetchURl={requests.requestPopular}/>
+      <Row rowID='3' title='Trending' fetchURl={requests.requestTrending}/>
+      <Row rowID='4' title='Top Rated' fetchURl={requests.requestTopRated}/>
+      <Row rowID='5' title='Horror' fetchURl={requests.requestHorror}/>
     </div>
   )
 }
